8432e3d1d85d5e7304dca5fe27a581544c27c4dd
nimco2298 <39317438+nimco2298@users.noreply.github.com>
2020-03-18 19:59:58 -0500

    Added createOrder() for OrderController to call

--- app/Model.php
+++ app/Model.php
@@ -1,5 +1,9 @@
 <?php
 
+/*Purpose of this class is to make insert,update, or delete info to our DB 
+ * 
+ * This class includes methods that deal with User functions and Order functions
+ */
 class Model{
     private $servername = "localhost";
     private $username = "root";
@@ -13,6 +17,26 @@ class Model{
     }
    
   
+    //Adds the order a Doctor makes to the DB
+    public function createOrder($order_id,$order_creationdate,$doctor_id,$patient_id,$med_id,$med_type, $med_qty, $med_unit){
+
+        $this->conn = new mysqli($this->servername, $this->username, $this->password, $this->dbname);
+
+        //INclude ordercreationdate in 
+        $sql = "INSERT INTO order(order_id,doctor_id, patient_id, caregiver_id,order_creationdate) values('$order_id''$doctor_id', '$patient_id',
+         '00000',$order_creationdate)";
+
+         //Add a Loop for each medication to be entered for 1 single orderID
+         //adminster_time can be left as optional right?
+        $sql = "INSERT INTO break_down(order_id,medication_id,med_ quantity,) values('$order_id', '$med_id', '$med_qty')";
+        
+        if(!mysqli_query($this->conn, $sql)){
+            return false;
+        }else{
+            return true;    
+        }
+    }
+
     public function addUser($first, $last, $role, $active) {
         
         $this->conn = new mysqli($this->servername, $this->username, $this->password, $this->dbname);
@@ -85,4 +109,3 @@ $result = $md->getCurrentView();
 
 echo $result;
 ?>
-  
