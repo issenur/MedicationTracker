9656ff55f8695d33ea92f8af0f438c0b09d65d26
Ryan <ryanbradleympls@gmail.com>
2020-04-23 17:22:08 -0500

    Caregiver
    Updated caregiver links and added breadcrumbs to each use case.
    
    Created caregiver.php
    Updated caregiverfulfillsorderview.php to pull from database correctly, but it is still pulling from 0000. Needs to take user/caregiver ID from active session still.

--- app/CaregiverDashboardView.php
+++ app/CaregiverDashboardView.php
@@ -1,10 +1,10 @@
 <?php
-    
+
     session_start();
     if(!isset($_SESSION['username']) || $_SESSION['role'] != "caregiver"){
         header("location:index.php");
     }
-    
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -27,38 +27,35 @@
 <body class="hold-transition sidebar-mini layout-fixed layout-navbar-fixed layout-footer-fixed">
 <div class="wrapper">
     <nav class="main-header navbar navbar-expand navbar-white navbar-light">
-        <!-- Left navbar links -->
-        <ul class="navbar-nav">
-            <li class="nav-item">
-                <a class="nav-link" data-widget="pushmenu" href="#"><i class="fas fa-bars"></i></a>
-            </li>
-            <li class="nav-item d-none d-sm-inline-block">
-                <a href="#" class="nav-link">Home</a>
-            </li>
-            <li class="nav-item d-none d-sm-inline-block">
-                <a href="#" class="nav-link">Contact</a>
-            </li>
-        </ul>
-        
-        <!-- SEARCH FORM -->
-        <form class="form-inline ml-3">
-            <div class="input-group input-group-sm">
-                <input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
-                <div class="input-group-append">
-                    <button class="btn btn-navbar" type="submit">
-                    <i class="fas fa-search"></i>
-                    </button>
-                </div>
-            </div>
-        </form>
-    
-        <!-- Right navbar links -->
-        <ul class="navbar-nav ml-auto">
-            <li class="nav-item d-none d-sm-inline-block">
-                <a href="logout.php" class="nav-link">Logout</a>
-            </li>
-        </ul>
-    </nav>
+      <!-- Left navbar links -->
+      <ul class="navbar-nav">
+          <li class="nav-item">
+              <a class="nav-link" data-widget="pushmenu" href="#"><i class="fas fa-bars"></i></a>
+          </li>
+          <li class="nav-item d-none d-sm-inline-block">
+              <a href="#" class="nav-link">Home</a>
+          </li>
+      </ul>
+
+      <!-- SEARCH FORM -->
+      <form class="form-inline ml-3">
+          <div class="input-group input-group-sm">
+              <input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
+              <div class="input-group-append">
+                  <button class="btn btn-navbar" type="submit">
+                  <i class="fas fa-search"></i>
+                  </button>
+              </div>
+          </div>
+      </form>
+
+      <!-- Right navbar links -->
+      <ul class="navbar-nav ml-auto">
+          <li class="nav-item d-none d-sm-inline-block">
+              <a href="logout.php" class="nav-link">Logout</a>
+          </li>
+      </ul>
+  </nav>
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
--- app/CaregiverFulfillsOrderView.php
+++ app/CaregiverFulfillsOrderView.php
@@ -29,46 +29,81 @@ global $model;
   <nav class="main-header navbar navbar-expand navbar-white navbar-light">
     <!-- Left navbar links -->
     <ul class="navbar-nav">
-      <li class="nav-item">
-        <a class="nav-link" data-widget="pushmenu" href="#"><i class="fas fa-bars"></i></a>
-      </li>
-      <li class="nav-item d-none d-sm-inline-block">
-        <a href="CaregiverDashboard.html" class="nav-link">Home</a>
-      </li>
-      <li class="nav-item d-none d-sm-inline-block">
-        <a href="/index.php" class="nav-link">Log Out</a>
-      </li>
+        <li class="nav-item">
+            <a class="nav-link" data-widget="pushmenu" href="#"><i class="fas fa-bars"></i></a>
+        </li>
+        <li class="nav-item d-none d-sm-inline-block">
+            <a href="http://localhost/CaregiverDashboardView.php" class="nav-link">Home</a>
+        </li>
     </ul>
 
-        <!-- Right navbar links -->
-        <ul class="navbar-nav ml-auto">
-            <!-- Messages Dropdown Menu -->
-            <li class="nav-item dropdown">
-            </li>
-            <!-- Notifications Dropdown Menu -->
-            <li class="nav-item dropdown">
-            </li>
-            <li class="nav-item">
-            </li>
-        </ul>
-    </nav>
-    <!-- /.navbar -->
+    <!-- SEARCH FORM -->
+    <form class="form-inline ml-3">
+        <div class="input-group input-group-sm">
+            <input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
+            <div class="input-group-append">
+                <button class="btn btn-navbar" type="submit">
+                <i class="fas fa-search"></i>
+                </button>
+            </div>
+        </div>
+    </form>
 
+    <!-- Right navbar links -->
+    <ul class="navbar-nav ml-auto">
+        <li class="nav-item d-none d-sm-inline-block">
+            <a href="logout.php" class="nav-link">Logout</a>
+        </li>
+    </ul>
+</nav>
     <!-- Main Sidebar Container -->
     <aside class="main-sidebar sidebar-dark-primary elevation-4">
+        <!-- Brand Logo -->
+        <a href="CaregiverDashboardView.php" class="brand-link">
+            <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+            style="opacity: .8">
+            <span class="brand-text font-weight-light">MedicationTracker</span>
+        </a>
 
         <!-- Sidebar -->
         <div class="sidebar">
-            <!-- Sidebar user (optional) -->
+            <!-- Sidebar user panel (optional) -->
             <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+                <div class="image">
+                    <img src="dist/img/caregiverimage.png" class="img-circle elevation-2" alt="User Image">
+                </div>
                 <div class="info">
-                    <a href="#" class="d-block">SESSION: Caregiver</a>
+                    <a href="#" class="d-block">Role: Caregiver</a>
                 </div>
             </div>
 
             <!-- Sidebar Menu -->
             <nav class="mt-2">
                 <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+                    <!-- Add icons to the links using the .nav-icon class
+                    with font-awesome or any other icon font library -->
+                    <li class="nav-item has-treeview menu-open">
+                        <ul class="nav nav-treeview">
+                            <li class="nav-item">
+                                <a href="./CaregiverDashboardView.php" class="nav-link">
+                                    <i class="far fa-circle nav-icon"></i>
+                                    <p>Caregiver Dashboard</p>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="./CaregiverClaimsOrderView.php" class="nav-link">
+                                    <i class="far fa-circle nav-icon"></i>
+                                    <p>Self-Assign Order</p>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="./CaregiverFulfillsOrderView.php" class="nav-link active">
+                                    <i class="far fa-circle nav-icon"></i>
+                                    <p>Fulfill Order</p>
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
                 </ul>
             </nav>
             <!-- /.sidebar-menu -->
@@ -78,28 +113,33 @@ global $model;
 
     <!-- Content Wrapper. Contains page content -->
     <div class="content-wrapper">
+      <!-- Content Header (Page header) -->
+      <div class="content-header">
+        <div class="container-fluid">
+          <div class="row mb-2">
+            <div class="col-sm-6">
+              <h1 class="m-0 text-dark">Fulfill Assigned Orders</h1>
+            </div><!-- /.col -->
+            <div class="col-sm-6">
+              <ol class="breadcrumb float-sm-right">
+                <li class="breadcrumb-item"><a href="http://localhost/CaregiverDashboardView.php">Home</a></li>
+                <li class="breadcrumb-item active">Fulfill Order</li>
+              </ol>
+            </div><!-- /.col -->
+          </div><!-- /.row -->
+        </div><!-- /.container-fluid -->
+      </div>
+
         <!-- Content Header (Page header) -->
         <section class="content-header">
-          <div class="card">
-            <div class="card-header border-transparent">
-              <h3 class="card-title"><b>FULFILL ORDERS</b></h3>
-
-              <div class="card-tools">
-                <button type="button" class="btn btn-tool" data-card-widget="collapse">
-                  <i class="fas fa-minus"></i>
-                </button>
-                <button type="button" class="btn btn-tool" data-card-widget="remove">
-                  <i class="fas fa-times"></i>
-                </button>
-              </div>
-            </div>
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-12">
                        <table id="example2" class="table table-bordered table-hover">
                             <thead>
                                 <tr>
                                     <th>OrderID</th>
                                     <th>Patient</th>
-                                    <th>Medication</th>
-                                    <th>Units</th>
                                     <th>Date</th>
                                     <th>Fulfill</th>
                                 </tr>
@@ -112,19 +152,26 @@ global $model;
                                     die("Connection failed: " . $conn->connect_error);
                                 }
 
-                                $sql = "SELECT * FROM `order` WHERE `order_id` = 6458";
+                                $sql = "SELECT";
+                                $sql .= "`order`.`order_id` AS `order_id` ,";
+                                $sql .= "`patient`.`first` AS `pfirst` ,";
+                                $sql .= "`patient`.`last` AS `plast` ,";
+                                $sql .= " DATE_FORMAT(`date`, '%d-%b-%Y') AS `date`,";
+                                $sql .= " `order`.`patient_id` AS `patient_id`";
+                                $sql .= " FROM `order` ";
+                                $sql .= " JOIN `patient` ON (`patient`.`patient_id` = `order`.`patient_id`)";
+                                $sql .= " WHERE `care_giver_id` = 0000";
                                 $result = $conn->query($sql);
                                 echo "<id='example2'>";
                                 echo "<tbody>";
                                 if ($result->num_rows > 0) {
                                     while($row = $result->fetch_assoc()) {
                                         echo "<tr>";
-                                        echo "<td>" . $row['order_id'] . "</td>";
-                                        echo "<td>" . $row['patient_id'] . "</td>";
-                                        echo "<td>" . $row['medication_id'] . "</td>";
-                                        echo "<td>" . $row['units'] . "</td>";
+                                        echo "<td>" . (int)$row['order_id'] . "</td>";
+                                        echo "<td>" . $row['pfirst'] . " " .  $row['plast'] . "</td>";
                                         echo "<td>" . $row['date'] . "</td>";
-                                        echo "<a href ='CaregiverViewControllerHelper.php?fulfill_order=".  $row['order_id'] ."'><button class='btn-flat btn-secondary'>Fulfill</button>"."<a/>";
+                                        echo "<td>";
+                                        echo "<a href ='CaregiverCODetailView.php?claim_order=".  $row['order_id'] ."'><button class='btn btn-dark'>Fulfill Order</button>"."<a/>";
                                         echo "</td>";
                                         echo "</tr>";
                                     }
@@ -133,7 +180,7 @@ global $model;
                                 } else {
                                     echo "</tbody>";
                                     echo "</table>";
-                                    echo "<h4> ORDERS DATABASE EMPTY</h4>";
+                                    echo "<h4>ORDERS DATABASE EMPTY</h4>";
                                 }
                             ?>
                     </div>
